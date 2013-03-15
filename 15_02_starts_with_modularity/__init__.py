2816150b3b36cc69e2655f4fe1d374a31be4b97b
Dmi Baranov <dmi.baranov@gmail.com>
2013-03-15 11:57:58 -0700

    02 done

--- 15_02_starts_with_modularity/__init__.py
+++ 15_02_starts_with_modularity/__init__.py
@@ -1,4 +1,21 @@
-#1
+#3 prems:
+#1. types for nouns
+#2.express ourselves sctrlly - packages sctr & code
+#3. ost of parametric progrmng
+#
+#yah, spaggety!
+#possible way - composition. NOTE: near w/ inht haos
+#i-faces overlapping
+#
+#fault tolerance
+#ok - callng imm. attentios
+#so bad - silent errors
+#right way - broke code in small methos and objects
+#
+#modules tranf to collections of types and functs
+#*provide of powerful factory for subclassing creatings (A -> B) -> C
+#
+#ßstatless adatpers in very good anf provides is very clear  high-lvel code
 
 if __name__ == '__main__':
     print 'the ends with object inhtce and starts with modularity'
