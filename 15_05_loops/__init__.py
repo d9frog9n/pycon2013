8a19d2d8c117fede2e6c1ab35586ebda6c7bf025
Dmi Baranov <dmi.baranov@gmail.com>
2013-03-15 14:49:21 -0700

    5 in-progress

---
+++ 15_05_loops/__init__.py
@@ -0,0 +1,14 @@
+#http://nedbatchelder.com/text/iter.html
+#iteractions
+#while loop
+#for loop
+##COOL TRICK use for iter for regex matches
+##same for os.walk
+##and itertools
+#common tools for iteractions
+##enumerate
+##zip
+##max function accepting key params
+#custom iteractions
+#generators
+#
