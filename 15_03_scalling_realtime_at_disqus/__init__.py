19e9ae833765c3eb4aeb35408691fc1424d65597
Dmi Baranov <dmi.baranov@gmail.com>
2013-03-15 12:38:49 -0700

    3 done

---
+++ 15_03_scalling_realtime_at_disqus/__init__.py
@@ -0,0 +1,4 @@
+# https://speakerdeck.com/northisup/krtconf-making-disqus-realtime
+#use python anf some other techs you know:)
+#used haproxy, but leave to nginx + push stream module (https://github.com/wandenberg/nginx-push-stream-module)
+#end point - https://github.com/andyet/thoonk.py
