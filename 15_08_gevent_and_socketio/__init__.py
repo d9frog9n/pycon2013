04a5ec2dfff9ba096e5c9d0833f259ad914c70fc
Dmi Baranov <dmi.baranov@gmail.com>
2013-03-15 18:59:12 -0700

    socketio

---
+++ 15_08_gevent_and_socketio/__init__.py
@@ -0,0 +1,7 @@
+#https://github.com/lukesneeringer/pycon2013-socketio/
+#ajax - client-driven
+#socket.io clienbt
+#lie with namespaces
+#socket.io - two main parts on client - emit (wtf - sends data) and on - get data
+#socket.io server (event impl)
+#gevent-socketio
