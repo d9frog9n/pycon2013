91e560994154fd58bbe7ff0fc203565cb79458eb
Dmi Baranov <dmi.baranov@gmail.com>
2013-03-15 17:11:41 -0700

    done 7

--- 15_07_beatuful_code/__init__.py
+++ 15_07_beatuful_code/__init__.py
@@ -1,11 +1,38 @@
-#againt about builtins - max, min, enumerate
+#again about builtins - max, min, enumerate
+#
 #list looping
 #iterators vs lists
 #lol: renamed xrange in python3
 #collections looping - use magic methon __iter__ to implß
 #reverse iteration - reversed() builtin - accepts and return iterator
+#
 #zip for looping few collections
 #dont forget about izip
+#
 #sorting
 #sorted/sorted(...,reverse,key,cmp...)
+#exit points
+#
+#dict tools
+#keys, values, items, itemimets/values
+#counting by dicts
+#grouping by dicts
+#
+#impr clarity
+##use a keyword args
+##return a multiple values by tutples/namedtuples
+##unpack seq by a, b, c = 0, 1, 2- not indexing (dont forget abount typos in index)
+##same for updating x, y = 0, 1 vs x = 0; y = 0
+##a, b = b, a
+#
+#decorators and context managers
+##helps to isolate bizlogic
+#
+#locks
+#
+#contex manager - ignore - EXCEPT ALL!1
+#named context managers
+#
+#one-liners
+#new Raymond's rule - one line of code == one eglish sentence
 #
